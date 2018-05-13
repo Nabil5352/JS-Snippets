2ba4e6bf9961c94d4aea1df37d23c9f66a526f4e
Nabil5352 <report.nabil@gmail.com>
2018-05-13 21:21:29 +0600

    solved deep comparison

--- deep-comparison.js
+++ deep-comparison.js
@@ -17,3 +17,53 @@ typeof null also produces "object".
 
 */
 
+Object.size = function (obj){
+	size = 0;
+	for (key in obj){
+		if(obj.hasOwnProperty(key)) size++;
+	}
+	return size;
+}
+
+function deepEqual(value1, value2){
+	if ( value1 !== null || value2 !== null ){
+		if ( typeof value1 === "object" && typeof value2 === "object" ){
+			if(Object.size(value1) == Object.size(value2)){
+				count = 0;
+				for(key in value1){
+					if(value2.hasOwnProperty(key) && value1[key] == value2[key]) count++;
+				}
+				if(count == Object.size(value1)){
+					console.log("Equal");
+				}else{
+					console.log("Not Equal");
+				}
+			}else{
+				console.log("Not Equal");
+			}
+		}
+	}
+}
+
+var obj = {here: {is: "an"}, object: 2};
+
+var x = {a: 1, b: 2}
+var y = {b: 2, a: 1}
+
+var m = {a: 1, b: 2}
+var n = {b: 1, a: 2}
+
+var a = {a: 1, b: 2, c: 3}
+var b = {b: 1, a: 2}
+
+var e = {a: null, b: 2}
+var f = {b: 1, a: 2}
+
+deepEqual(x, y)
+deepEqual(m, n)
+deepEqual(a, b)
+deepEqual(e, f)
+
+deepEqual(obj, obj);
+deepEqual(obj, {here: 1, object: 2});
+deepEqual(obj, {here: {is: "an"}, object: 2});
